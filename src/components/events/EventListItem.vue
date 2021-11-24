0e05e9761d6c175ad9ac58b132c4884a1f75de84
Lasse17 <lasse@finetech.info>
2021-11-24 15:56:16 +0100

    Fixing eventlis

--- src/components/events/EventListItem.vue
+++ src/components/events/EventListItem.vue
@@ -4,13 +4,11 @@
       <h2 class="event-list-item__title">
         {{ event.Eventname }}
       </h2>
-
-     <div class="event-list-item__location">
+      <div class="event-list-item__location">
         {{ event.location }}
       </div>
-        <div class="event-list-item__notes">
+      <div class="event-list-item__date">
         {{ event.Date }}
-
       </div>
     </div>
   </div>
