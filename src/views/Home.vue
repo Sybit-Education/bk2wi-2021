68bf2e9d066f2cfb08919ea7ff9417164f0b9371
Lasse17 <lasse@finetech.info>
2021-12-08 15:24:20 +0100

    erster teil der suchleiste

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,9 @@
     class="home"
   >
     <h1>Home</h1>
+   <b-input-group size="lg" >
+    <b-form-input placeholder="Serach..." type="search"></b-form-input>
+  </b-input-group>
     <event-list   />
   </div>
 </template>
