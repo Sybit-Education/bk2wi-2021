b580f8043530877382a35cc062d403b72baae331
Lluca-st <luca.strecker@web.de>
2021-12-15 14:50:51 +0100

    Bilderkarussell angepasst, Text entfernt

--- src/views/EventDetail.vue
+++ src/views/EventDetail.vue
@@ -15,34 +15,11 @@
       @sliding-start="onSlideStart"
       @sliding-end="onSlideEnd"
     >
-      <!-- Text slides with image -->
-      <b-carousel-slide
-        caption="First slide"
-        text="Nulla vitae elit libero, a pharetra augue mollis interdum."
-        img-src="https://picsum.photos/1024/480/?image=52"
-      ></b-carousel-slide>
-
-      <!-- Slides with custom text -->
-      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54">
-        <h1>Hello world!</h1>
-      </b-carousel-slide>
-
-      <!-- Slides with image only -->
+      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=52"></b-carousel-slide>
+      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54"></b-carousel-slide>
+      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=55"></b-carousel-slide>
       <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=58"></b-carousel-slide>
 
-      <!-- Slides with img slot -->
-      <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
-      <b-carousel-slide>
-        <template #img>
-          <img
-            class="d-block img-fluid w-100"
-            width="1024"
-            height="480"
-            src="https://picsum.photos/1024/480/?image=55"
-            alt="image slot"
-          >
-        </template>
-      </b-carousel-slide>
     </b-carousel>
 
         <b-col>Ort: {{ event.location }}</b-col>
