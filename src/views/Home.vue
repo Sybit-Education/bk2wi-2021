ed978c62f12b0dfc0ab5554601ab74168bbbe79b
Lasse17 <lasse@finetech.info>
2021-12-15 15:09:57 +0100

    verbesserung der suchleiste

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,10 +4,14 @@
     class="home"
   >
     <h1>Home</h1>
-   <SearchBar hint="Search hint" :text="searchPhrase" @textChange="onTextChanged" @submit="onSubmit" />
-
-
-<SearchBar v-model="searchQuery" />
+   <div>
+  <b-navbar type="light" variant="light">
+    <b-nav-form>
+      <b-form-input class="mr-sm-2" placeholder="Search"></b-form-input>
+      <b-button variant="outline-success" class="my-2 my-sm-0" type="submit">Search</b-button>
+    </b-nav-form>
+  </b-navbar>
+</div>
     <event-list   />
   </div>
 </template>
