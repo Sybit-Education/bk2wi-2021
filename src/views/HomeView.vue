3c3755fd58430a93c6c021a3d1f7ff24d69773aa
Lasse17 <lasse@finetech.info>
2022-01-12 16:12:09 +0100

    änderung der suchleiste

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,13 +5,6 @@
   >
     <h1>Home</h1>
    <div>
-  <b-navbar type="light" variant="light">
-    <b-nav-form>
-      <b-form-input class="mr-sm-2" placeholder="Search"></b-form-input>
-      <b-button variant="outline-success" class="my-2 my-sm-0" type="submit">Search</b-button>
-    </b-nav-form>
-  </b-navbar>
-</div>
     <event-list   />
   </div>
 </template>
