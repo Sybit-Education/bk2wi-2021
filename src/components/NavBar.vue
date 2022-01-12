36e8958f254855e8648945657cfb61b349e76e40
Lasse17 <lasse@finetech.info>
2022-01-12 15:03:39 +0100

    umänderun der suchleite in die Navigations leiste

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -28,6 +28,12 @@
           <b-nav-item to="/about">
             Über
           </b-nav-item>
+              <b-navbar type="light" variant="light">
+                <b-nav-form>
+                    <b-form-input class="mr-sm-2" placeholder="Search"></b-form-input>
+                    <b-button variant="outline-success" class="my-2 my-sm-0" type="submit">Search</b-button>
+                </b-nav-form>
+              </b-navbar>
         </b-navbar-nav>
       </b-collapse>
     </b-container>
