9d0749f0dd579b1fd9de77326c95d637acd5c9f4
Panuzi <arjan-gta@hotmail.comm>
2022-01-26 13:58:22 +0100

    Detail page optically improved

--- src/views/EventDetailView.vue
+++ src/views/EventDetailView.vue
@@ -1,57 +1,35 @@
 <template>
   <div class="event-detail">
-    <b-overlay
-      :show="isLoading"
-      rounded="sm"
-      no-wrap
-    />
+    <b-overlay :show="isLoading" rounded="sm" no-wrap />
     <div v-if="event">
+
+  <b-img src="https://via.placeholder.com/150?text=Bild"
+        fluid-grow alt="Fluid-grow image">
+        </b-img>
+
       <h1>{{ event.Eventname }}</h1>
 
-      <b-carousel
-        id="event-detail"
-        v-model="slide"
-        :interval="4000"
-        controls
-        indicators
-        background="#ababab"
-        img-width="1024"
-        img-height="480"
-        style="text-shadow: 1px 1px 2px #333"
-        @sliding-start="onSlideStart"
-        @sliding-end="onSlideEnd"
-      >
-        <b-carousel-slide
-          img-src="https://picsum.photos/1024/480/?image=52"
-        />
-        <b-carousel-slide
-          img-src="https://picsum.photos/1024/480/?image=54"
-        />
-        <b-carousel-slide
-          img-src="https://picsum.photos/1024/480/?image=55"
-        />
-        <b-carousel-slide
-          img-src="https://picsum.photos/1024/480/?image=58"
-        />
-      </b-carousel>
-
-      <b-row>
-        <b-col>Ort: {{ event.location }}</b-col>
-        <b-col>Datum: {{ event.Date }}</b-col>
-        <b-col>Beschreibung: {{ event.Description }}</b-col>
-      </b-row>
-      <b-row>
-        <b-button
-          variant="success"
-          href="/"
-          target="_self"
-        >
-          Zurück zur Startseite
-        </b-button>
-      </b-row>
-    </div>
+          <b-col>
+            <div class="event-list-item__date">Datum: {{ event.Date }}</div>
+            <div class="event-list-item__Time">Uhrzeit: {{ event.Time }}</div>
+            <div class="event-list-item__location">Wo: {{ event.location }}</div>
+            </b-col> 
+        <br>
+          <b-col>
+            <div class="event-list-item_description">{{ event.Description }}</div>
+          </b-col>
+        <br>
+          <b-col> 
+            <div class="event-list-item_organizer">Veranstallter: {{ event.FirstName }} {{ event.Name }} </div>
+          </b-col>
+          <br>
+        <b-row>
+          <b-button variant="success" href="/" target="_self">Zurück zur Startseite</b-button>
+        </b-row>
+      </div>
   </div>
 </template>
+
 
 <script>
 import eventService from '@/services/event.service'
