00a922de6367b25e4ff4f0d54df8aea116006286
Panuzi <arjan-gta@hotmail.comm>
2022-01-26 17:24:10 +0100

    Plus icon minimal verkleinert im Homeview

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -27,7 +27,7 @@
   z-index: 10;
   bottom: 0.5rem;
   border-radius: 50%;
-  font-size: 3rem;
+  font-size: 2.85rem;
   padding: 0;
   box-shadow: 0 3px 5px -1px rgba(0, 0, 0, 0.2), 0 6px 10px 0 rgba(0, 0, 0, 0.14), 0 1px 18px 0 rgba(0, 0, 0, 0.12);
 }
