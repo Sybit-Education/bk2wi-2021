f743b53c1b1b55dff4fc6fac5d81547ea22fe818
Panuzi <arjan-gta@hotmail.comm>
2022-02-16 16:25:00 +0100

    about mit Text befühlt

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,7 +3,11 @@
     v-once
     class="about"
   >
-    <h1>Über</h1>
+    <h1>Über uns:</h1>
+
+<p>Wir, das Events4u Team sind eine engagierte Gruppe von Schülern, welche sich in der Klasse zusammengetan haben, um mit unserem gemeinsamen Know-How eine App/Website zu entwickeln.
+Im Herbst des Jahres 2021 wurde Events4u von uns gegründet um den Leuten die spontan bock auf’s feiern haben, eine helfende Hand zu reichen. Unsere App bietet eine Möglichkeit Events wie Feiern, Festivals und Geburtstage in der Umgebung von einem zu sehen und daran teilzunehmen.
+Für uns steht ihr, die die bei uns nach Events suchen im Mittelpunkt, deshalb haben wir uns auch für den Namen Events4u entschieden.</p>
   </div>
 </template>
 
