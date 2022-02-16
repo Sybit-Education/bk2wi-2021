ab185dfe65dbca487bca1a4035eb61df8f9feaf1
Lasse17 <lasse@finetech.info>
2022-02-16 16:03:03 +0100

    tag daten erhalten

--- src/views/ImprintView.vue
+++ src/views/ImprintView.vue
@@ -6,29 +6,36 @@
     <h1>Impressum</h1>
 
     <h2>Angaben gemäß § 5 TMG</h2>
-    <p>Events{4u}<br />
-    Allemannenstraße 15<br />
-    78315 Radolfzell</p>
+    <p>
+      Events{4u}<br>
+      Allemannenstraße 15<br>
+      78315 Radolfzell
+    </p>
 
-    <p>Vertreten durch:<br />
-    -</p>
+    <p>
+      Vertreten durch:<br>
+      -
+    </p>
 
     <h2>Kontakt</h2>
 
-    <p>Telefon:<br />
-    E-Mail: evets4u.schule@gmail.com</p>
+    <p>
+      Telefon:<br>
+      E-Mail: evets4u.schule@gmail.com
+    </p>
 
     <h2>Redaktionell verantwortlich</h2>
     <p>Simon C.</p>
 
     <h2>EU-Streitschlichtung</h2>
-    <p>Die Europäische Kommission stellt einer Plattform zur Online-Streitbeteiligung (OS) bereit:<br />
-    https://ec.europa.eu/consumers/odr/.<br />
-    Unsere E-Mail-Adresse finden Sie oben im Impressum.</p>
+    <p>
+      Die Europäische Kommission stellt einer Plattform zur Online-Streitbeteiligung (OS) bereit:<br>
+      https://ec.europa.eu/consumers/odr/.<br>
+      Unsere E-Mail-Adresse finden Sie oben im Impressum.
+    </p>
 
     <h2>Verbraucherstreitbeilegung/Universalschlichtungsstelle</h2>
     <p>Wir sind nicht bereit oder verpflichtet, an Streitbeteiligungsverfahren oder einer Verbraucherschlichtungsstelle teilzunehmen.</p>
-
   </div>
 </template>
 
